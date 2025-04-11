bba1c1994e46d2c1faff925eff483f18a527ec2a
Aydon Phillips <aydonphillips@outlook.com>
2025-04-10 20:05:19 -0500

    Remove call to clear in set_clear_color

--- src/graphics/opengl.cpp
+++ src/graphics/opengl.cpp
@@ -56,7 +56,6 @@ namespace OpenGL {
 
     void Context::set_clear_color(float r, float g, float b, float a){
         glClearColor(r, g, b, a);
-        glClear(GL_COLOR_BUFFER_BIT);
     }
 
     void Context::sync(){
