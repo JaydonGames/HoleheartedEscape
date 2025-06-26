61ee1c7e1be50e5f398e4d1b9a0a2154abae5fd6
JosephArifin <josepharifin03@gmail.com>
2025-06-26 17:01:27 -0500

    feat: unary - operator on vec2

--- include/graphics/types.hpp
+++ include/graphics/types.hpp
@@ -2,60 +2,66 @@
 
 #include <cmath>
 namespace Math {
-    
+
     template<typename T>
     struct Vec2 {
-            T x, y;
-            Vec2(T x = 0, T y = 0) : x(x), y(y) {}
-            Vec2 operator+(const Vec2& other) const {
-                return Vec2(x + other.x, y + other.y);
+        T x, y;
+        Vec2(T x = 0, T y = 0)
+            : x(x),
+              y(y) {}
+        Vec2 operator+(const Vec2& other) const {
+            return Vec2(x + other.x, y + other.y);
+        }
+
+        Vec2 operator-(const Vec2& other) const {
+            return Vec2(x - other.x, y - other.y);
+        }
+
+        Vec2 operator*(T scalar) const {
+            return Vec2(x * scalar, y * scalar);
+        }
+
+        Vec2 operator/(T scalar) const {
+            return Vec2(x / scalar, y / scalar);
+        }
+
+        Vec2 operator-() const {
+            return Vec2(x * -1.0f, y * -1.0f);
+        }
+
+        T dot_product(const Vec2& other) const {
+            return T(x * other.x + y * other.y);
+        }
+
+        friend inline Vec2 operator*(T scalar, const Vec2 vec) {
+            return vec * scalar;
+        }
+
+        Vec2 get_perpendicular() const {
+            return Vec2(-y, x);
+        }
+
+        Vec2 normalize() {
+            double mag_sq = x * x + y * y;
+            if (mag_sq == 0.0) {
+                return Vec2(0.0, 0.0);
             }
 
-            Vec2 operator-(const Vec2& other) const {
-                return Vec2(x - other.x, y - other.y);
-            }
+            T inv_mag = 1.0 / std::sqrt(mag_sq);
 
-            Vec2 operator*(T scalar) const {
-                return Vec2(x * scalar, y * scalar);
-            }
-
-            Vec2 operator/(T scalar) const {
-                return Vec2(x / scalar, y / scalar);
-            }
-
-            T dot_product(const Vec2& other) const {
-                return T(x * other.x + y * other.y);
-            }
-
-            friend inline Vec2 operator*(T scalar, const Vec2 vec) {
-                return vec * scalar;
-            }
-
-            Vec2 get_perpendicular() const {
-                return Vec2(-y, x);
-            }
-
-            Vec2 normalize() {
-                double mag_sq = x * x + y * y;
-                if (mag_sq == 0.0) {
-                    return Vec2(0.0, 0.0);
-                }
-
-                T inv_mag = 1.0 / std::sqrt(mag_sq);
-
-                return Vec2(x * inv_mag, y * inv_mag);
-            }
+            return Vec2(x * inv_mag, y * inv_mag);
+        }
     };
 
     struct Color {
-            float r, g, b;
+        float r, g, b;
     };
 
     struct Vec4 {
-            int x, y, z, w;
+        int x, y, z, w;
     };
 
     struct Rect {
-            int x, y, w, h;
+        int x, y, w, h;
     };
 }  // namespace Render
