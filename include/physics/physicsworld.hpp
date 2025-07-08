1ea397954ef50fdb076aaf91ae2e109e16fb1b80
JosephArifin <josepharifin03@gmail.com>
2025-07-08 16:16:00 -0500

    changed Square data type to Shape

--- include/physics/physicsworld.hpp
+++ include/physics/physicsworld.hpp
@@ -3,35 +3,37 @@
 #include <vector>
 #include "graphics/types.hpp"
 #include "spatial_hashing_grid.hpp"
+#include "shape.hpp"
 #include "square.hpp"
 
 class PhysicsWorld {
-    public:
-        PhysicsWorld(SpatialHashingGrid);
+public:
+    PhysicsWorld(SpatialHashingGrid);
 
-        void add_object(Square* square);
+    void add_object(Shape* square);
 
-        Square* get_square(int i);
+    Shape* get_object(int i);
 
-        void update(double dt);
+    void update(double dt);
 
-    private:
-        SpatialHashingGrid spatial_grid;
-        std::vector<Square*> m_all_objects;
-        std::vector<Square*> m_nonstatic_objects;
-        std::vector<Square*> m_static_objects;
-        Math::Vec2<float> m_gravity;
-        const int sub_steps = 8;
+private:
+    SpatialHashingGrid spatial_grid;
+    std::vector<Shape*> m_all_objects;
+    std::vector<Shape*> m_nonstatic_objects;
+    std::vector<Shape*> m_static_objects;
+    Math::Vec2<float> m_gravity;
+    const int sub_steps = 8;
 
-        void update_positions(Square* square, double dt);
+    void update_positions(Shape* square, double dt);
 
-        void apply_gravity(Square* square);
+    void apply_gravity(Shape* square);
 
-        void satisfy_constraints(Square* square);
+    void satisfy_constraints(Shape* square);
 
-        void solve_collisions();
+    void solve_collisions();
 
-        void resolve_collision(auto& object_1, auto& object_2, std::vector<VerletParticle*> colliding_particles_1, std::vector<VerletParticle*> colliding_particles_2, Math::Vec2<float> normal, double depth);
+    void resolve_collision(auto& object_1, auto& object_2, std::vector<VerletParticle*> colliding_particles_1,
+                           std::vector<VerletParticle*> colliding_particles_2, Math::Vec2<float> normal, double depth);
 
-        std::vector<VerletParticle*> get_collision_particles(std::array<VerletParticle*, 4>, Math::Vec2<float> normal);
+    std::vector<VerletParticle*> get_collision_particles(std::array<VerletParticle*, 4>, Math::Vec2<float> normal);
 };
