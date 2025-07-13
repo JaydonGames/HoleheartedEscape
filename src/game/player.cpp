2092a610dc1354e1a1dca384c712edb14b824204
JosephArifin <josepharifin03@gmail.com>
2025-07-13 01:05:38 -0500

    player: added function to fix player holding up to fly

--- src/game/player.cpp
+++ src/game/player.cpp
@@ -2,23 +2,26 @@
 #include <SDL_events.h>
 #include <SDL_keyboard.h>
 #include "graphics/types.hpp"
+#include "structures.hpp"
+#include <algorithm>
 #include <cstdlib>
+#include <limits>
 
 Math::Vec2<float> Player::PLAYER_JUMP_FORCE;
 
 Player::Player(Math::Vec2<float> pos, float mass, float side_length)
-    : Square(pos, mass, side_length) {
+    : Square(pos, mass, side_length, false, true) {
     PLAYER_JUMP_FORCE = Math::Vec2<float>(0, -15000.0f);
-    is_jumping = false;
+    m_is_jumping = false;
 };
 
 void Player::input() {
     const Uint8* keystate = SDL_GetKeyboardState(NULL);
 
-    if (!is_jumping) {
+    if (!m_is_jumping) {
         if (keystate[SDL_SCANCODE_UP]) {
             apply_force(PLAYER_JUMP_FORCE);
-            // is_jumping = true;
+            m_is_jumping = true;
         }
     }
     if (keystate[SDL_SCANCODE_LEFT]) {
@@ -27,5 +30,17 @@ void Player::input() {
 
     if (keystate[SDL_SCANCODE_RIGHT]) {
         apply_force(Math::Vec2<float>(PLAYER_SPD, 0));
+    }
+}
+
+void Player::check_to_enable_player_jump(ArrayRef<VerletParticle> collided_object_particles) {
+    // NOTE: A higher 'height' = lower y value, more negative
+    float other_highest_y = std::numeric_limits<float>::infinity();
+    for (VerletParticle particle : collided_object_particles) {
+        other_highest_y = std::min(other_highest_y, particle.curr_position.y);
+    }
+
+    if (other_highest_y > get_center_position().y) {
+        m_is_jumping = false;
     }
 }
