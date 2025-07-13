14e17f12b1ed575449920bf91bd2bbc77af3048b
JosephArifin <josepharifin03@gmail.com>
2025-07-13 01:01:06 -0500

    shape header: feat for player jump derivative class

--- include/physics/shape.hpp
+++ include/physics/shape.hpp
@@ -7,8 +7,9 @@ struct Shape {
     bool is_static;
     float mass;
     float angle;
+    bool is_player;
 
-    Shape(bool is_static, float mass);
+    Shape(bool is_static, float mass, bool is_player);
 
     virtual Math::Vec2<float> get_curr_position() = 0;
 
@@ -27,4 +28,9 @@ struct Shape {
     virtual void apply_force(Math::Vec2<float> f) = 0;
 
     virtual float get_angle() = 0;
+
+    // WARNING: This is terrible but I don't feel like rewriting everything lol
+    // It's only for the player class
+    // I also don't know how else to do this. Aydon help
+    virtual void check_to_enable_player_jump(ArrayRef<VerletParticle> collided_object_particles) = 0;
 };
