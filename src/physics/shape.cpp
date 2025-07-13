c8ffeae7df7204d75e0fa399abd4fdcc79f67e0c
JosephArifin <josepharifin03@gmail.com>
2025-07-13 01:08:08 -0500

    shape: constructor arg changes

--- src/physics/shape.cpp
+++ src/physics/shape.cpp
@@ -1,5 +1,6 @@
 #include "shape.hpp"
 
-Shape::Shape(bool is_static, float mass)
+Shape::Shape(bool is_static, float mass, bool is_player)
     : is_static(is_static),
+      is_player(is_player),
       mass(mass) {}
